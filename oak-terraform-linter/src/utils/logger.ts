88a5fdc3bc7823ee9f91ad9a89b92a3bad1e249b
Rob Ward <rob.ward@thenational.academy>
2026-03-18 15:15:27 +0000

    feat(linter): delete old code

--- oak-terraform-linter/src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-export const logger = {
-  error: (msg: string) => console.error(chalk.red(`✘ ${msg}`)),
-  warn: (msg: string) => console.warn(chalk.yellow(`⚠ ${msg}`)),
-  info: (msg: string) => console.log(chalk.cyan(`ℹ ${msg}`)),
-  success: (msg: string) => console.log(chalk.green(`✓ ${msg}`)),
-};
